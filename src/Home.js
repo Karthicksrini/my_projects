27267c6fea49a953d84e9c74284c6650cf281dcb
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2022-10-10 10:16:49 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,6 @@ function Home(){
             <h1>My Projects</h1>
        <a href="/calculator"> <div className="component">Calculator</div></a>
        <a href="/expense_tracker"> <div className="component">Expense Tracker</div></a>
-       <a href="/crud_operation"> <div className="component">CRUD Operation</div></a>
     
         </div>
     )
