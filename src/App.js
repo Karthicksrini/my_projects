cacd69f0f5a5784ad12c94bca39d45cd35ef03c2
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2022-10-10 10:17:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Calculator from "./Calculator";
 import Home from "./Home";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import ExpenseTracker from "./ExpenseTracker";
-import CrudOperation from "./CRUD_Operation";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/calculator" element={<Calculator/>}/>
       <Route path="/expense_tracker" element={<ExpenseTracker/>}/>
-      <Route path="/crud_operation" element={<CrudOperation/>}/>
       </Routes>
       </BrowserRouter>
      
