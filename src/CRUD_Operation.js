e7e8fa1d69ae7257a76dd7705a45c855d42a618e
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2022-10-10 10:17:17 +0530

    Delete CRUD_Operation.js

--- src/CRUD_Operation.js
+++
@@ -1,9 +0,0 @@
-function CrudOperation(){
-    return(
-        <>
-         <iframe title="CRUD_Operation" src="https://laughing-mclean-f186f4.netlify.app/"></iframe>
-        </>
-    )
-}
-
-export default CrudOperation;
